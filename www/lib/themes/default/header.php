c190859687359f3ffba3874cd749f96f9db214fd
Carlos Hugo Gonzalez Castell (Fundar) <carlos@fundar.org.mx>
2013-07-19 18:02:33 -0500

    js y css

--- www/lib/themes/default/header.php
+++ www/lib/themes/default/header.php
@@ -2,9 +2,13 @@
 <html lang="<?php print get("webLang"); ?>">
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		 <meta name="viewport" content="width=device-width" />
 		<title><?php print $this->getTitle(); ?></title>
 		<link href="<?php echo $this->themePath; ?>/css/auth-buttons.css" rel="stylesheet">
-		<link href="<?php echo $this->themePath; ?>/css/style.css" rel="stylesheet">
+		
+		  <link rel="stylesheet" href="<?php echo $this->themePath; ?>/css/foundation.css" />
+		<script src="<?php echo $this->themePath; ?>/js/vendor/custom.modernizr.js"></script>
+		
 		<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 	</head>
 	<body>
