315cf6a5dd7a9d1ab422e1f308c5387b85600fba
Carlos Hugo Gonzalez Castell (Fundar) <carlos@fundar.org.mx>
2013-07-18 19:03:23 -0500

    home view posts

--- www/applications/default/views/home.php
+++ www/applications/default/views/home.php
@@ -25,59 +25,58 @@
 	<p>
 		<a href="/logout" title="logout">logout</a>
 	</p>
-	
-	
-	<p>Posts</p>
-	
-	<?php if(is_array($posts)) { ?>
-		<?php foreach($posts as $post) { ?>		
-			<div class="post">
-				<p>
-					<a href="/reto/<?php echo utf8_decode($post["slug"]);?>" title="<?php echo utf8_decode($post["title"]);?>">
-						<img src="<?php echo get("webURL") . '/' . $post["image_url"];?>" alt="<?php echo $post["title"];?>">
-					</a>
-				</p>
-				
-				<p class="title">
-					<a href="/reto/<?php echo utf8_decode($post["slug"]);?>" title="<?php echo utf8_decode($post["title"]);?>">
-						<?php echo utf8_decode($post["title"]);?>
-					</a>
-				</p>
-				
-				<p class="abstract"><?php echo utf8_decode($post["abstract"]);?></p>
-				<p class="descr"><?php echo utf8_decode($post["descr"]);?></p>
-				<p class="descr">Categoria: <?php echo utf8_decode($post["category"]);?></p>
-				
-				<p>
-					<span class="like-post" value="<?php echo $post["post_id"];?>">Like</span>
-					&nbsp;<span class="votes vote<?php echo $post["post_id"];?>"><?php echo $post["votes"];?></span>
-				</p>
-			</div><br/><br/>
-		<?php } ?>
-		
-		<script type="text/javascript">
-			$(document).ready( function () {
-				$(".like-post").click( function () {
-					id = $(this).attr("value");
-					
-					$.ajax({
-						url: "/like/" + id
-					}).done(function (data) {
-						if(data == "false") {
-							
-						} else {
-							$(".vote" + id).text(data);
-						}
-					});
-				});
-			});
-		</script>
-	<?php } else { ?>
-		<p>se el primero en agregar un reto</p>
-	<?php } ?>
 <?php } else { ?>
 	<p>
 		<a class="btn-auth btn-github" href="/auth/github" title="Sign in with GitHub">Sign in with <b>GitHub</b></a>
 		<a class="btn-auth btn-twitter" href="/auth/twitter" title="Sign in with Twitter">Sign in with <b>Twitter</b></a>
 	</p>
+<?php } ?>
+
+<p>Posts</p>
+	
+<?php if(is_array($posts)) { ?>
+	<?php foreach($posts as $post) { ?>		
+		<div class="post">
+			<p>
+				<a href="/reto/<?php echo utf8_decode($post["slug"]);?>" title="<?php echo utf8_decode($post["title"]);?>">
+					<img src="<?php echo get("webURL") . '/' . $post["image_url"];?>" alt="<?php echo $post["title"];?>">
+				</a>
+			</p>
+			
+			<p class="title">
+				<a href="/reto/<?php echo utf8_decode($post["slug"]);?>" title="<?php echo utf8_decode($post["title"]);?>">
+					<?php echo utf8_decode($post["title"]);?>
+				</a>
+			</p>
+			
+			<p class="abstract"><?php echo utf8_decode($post["abstract"]);?></p>
+			<p class="descr"><?php echo utf8_decode($post["descr"]);?></p>
+			<p class="descr">Categoria: <?php echo utf8_decode($post["category"]);?></p>
+			
+			<p>
+				<span class="like-post" value="<?php echo $post["post_id"];?>">Like</span>
+				&nbsp;<span class="votes vote<?php echo $post["post_id"];?>"><?php echo $post["votes"];?></span>
+			</p>
+		</div><br/><br/>
+	<?php } ?>
+	
+	<script type="text/javascript">
+		$(document).ready( function () {
+			$(".like-post").click( function () {
+				id = $(this).attr("value");
+				
+				$.ajax({
+					url: "/like/" + id
+				}).done(function (data) {
+					if(data == "false") {
+						
+					} else {
+						$(".vote" + id).text(data);
+					}
+				});
+			});
+		});
+	</script>
+<?php } else { ?>
+	<p>se el primero en agregar un reto</p>
 <?php } ?>
