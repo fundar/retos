57cb0ad562d57799e8b07ac1e1e1ebe5d5ba6d92
Carlos Hugo Gonzalez Castell (Fundar) <carlos@fundar.org.mx>
2013-07-22 16:44:22 -0500

    footer links

--- www/lib/themes/default/footer.php
+++ www/lib/themes/default/footer.php
@@ -60,8 +60,8 @@
 		</div>
 		<div class="large-6 columns">
 		  <ul class="inline-list right code">
-			<li><a  href="#">Coded by FundarLabs</a></li>
-			<li><a  href="#">Designed by</a></li>
+			<li><a  href="http://fundar.org.mx" title="Fundar">Coded by FundarLabs</a></li>
+			<li><a  href="http://akora.mx" title="Akora">Designed by Akora</a></li>
 
 		  </ul>
 		</div>
