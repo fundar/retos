6a17c97434f5040fcff5f5734ab04a98e8f9c89e
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2013-07-22 03:17:27 -0500

    faqs

--- www/applications/default/views/faqs.php
+++ www/applications/default/views/faqs.php
@@ -5,7 +5,7 @@
 			<div class="large-12 columns ">	
 						
 				<p class="cita">
-					.........
+					Sí tienes más preguntas escríbenos a contacto@opendata.mx
 				</p>
 			
 				<div class="linea-quote"></div>
@@ -16,11 +16,11 @@
 			<!-- preguntas-->  
 			<div class="large-12 columns ">
 				<p class="question">&iquest;
-					Para qué un crear aplicaciones móviles en este reto si ya hay muchas actualmente?
+					Para qué crear aplicaciones móviles para la ciudad si ya hay muchas actualmente?
 				</p>
 				
 				<p>
-					La oferta actual de apps en materia de movilidad tienen un enfoque de muy corto plazo y no pueden generar valor agregado a partir de la reutilización de datos y metadatos relacionados. ConectaDF busca explorar la oportunidad de obtener beneficios de corto, mediano y largo plazo para la ciudadanía en general y no sólo de usuarios de smartphones.
+					La oferta actual de apps en materia de movilidad tiene dos limitantes intrínsecas: la primera es el enfoque de solución a problemáticas a muy corto plazo y la segunda es su ámbito limitado que beneficia exclusivamente a usuarios de smartphones. ConectaDF busca obtener beneficios de corto, mediano y largo plazo para la ciudadanía en general creando valor agregado a partir de la recopilación y análisis de datos y metadatos generados colectivamente. 
 				</p>
 				
 				<div class="linea-r"></div>
@@ -28,11 +28,11 @@
 			
 			<div class="large-12 columns ">
 				<p class="question">&iquest;
-					¿Puedo comercializar el desarrollo que envíe?
+					Se puede participar en equipo?
 				</p>
 				
 				<p>
-					Sí. De hecho las aplicaciones seleccionadas obtendrán acceso a un programa de incubación que garanticen su sustentabilidad y generen impacto de mediano y largo plazo. la única condición es que los datos generados a partir del uso de los desarrollos deberán estar disponibles como datos abiertos para las instituciones.
+					Sí, es posible participar en equipos de máximo 6 personas o individualmente.
 				</p>
 				
 				<div class="linea-r"></div>
@@ -40,14 +40,86 @@
 			
 			<div class="large-12 columns ">
 				<p class="question">&iquest;
-					... 					
+					Habrá premios?				
 				</p>
 				
 				<p>
-					...
+					Sí.  Los participantes cuyos proyectos sean seleccionados tendrán acceso a un programa de incubación/mentorship otorgado por las instituciones convocantes el cual permitirá perfeccionar el desarrollo, formar estrategias de adopción e implementación, así como garantizar acceso a infraestructura y recursos necesarios para maximizar su impacto. Dicho programa tendrá una duración mínima de 3 meses y máxima de un año contados a partir de la publicación del veredicto. 
 				</p>
 				
 				<div class="linea-r"></div>
-			</div>				
+			</div>
+			
+			<div class="large-12 columns ">
+				<p class="question">&iquest;
+					Cómo es el proceso de selección?
+				</p>
+				
+				<p>
+					Serán seleccionadas todas las aplicaciones que demuestren impacto potencial sobre la problemática particular a la que se dirigen, creatividad de la propuesta para la generación/análisis de datos, uso innovador de la tecnología, así como la calidad general de ejecución del desarrollo.
+				</p>
+				
+				<div class="linea-r"></div>
+			</div>
+			
+			<div class="large-12 columns ">
+				<p class="question">&iquest;
+					Puedo comercializar el desarrollo que envíe?
+				</p>
+				
+				<p>
+					Sí. De hecho las aplicaciones seleccionadas obtendrán acceso a un programa de incubación que garantice la sustentabilidad y la generación de impacto de mediano y largo plazo. La única condición  es que los datos generados por la adopción de los desarrollos deberán estar disponibles como datos abiertos para las instituciones convocantes y el público en general.
+				</p>
+				
+				<div class="linea-r"></div>
+			</div>
+			
+			<div class="large-12 columns ">
+				<p class="question">&iquest;
+					Es posible hacer modelos de negocio con datos abiertos?
+				</p>
+				
+				<p>
+					Existen <a href="http://www.red.es/reportajes/articles/id/5354/oportunidades-negocio-el-universo-open-data.html">varios</a> <a href="http://infonomics.ltd.uk/news/blog/2013/03/01/open-data-business-models/">antecedentes</a>, <a href="http://radar.oreilly.com/2013/01/open-data-business-models-deloitte-insight.html">ejemplos</a> y <a href="http://davidsasaki.name/2013/07/advice-for-open-data-startups/">consejos</a> al respecto.
+				</p>
+				
+				<div class="linea-r"></div>
+			</div>
+			
+			<div class="large-12 columns ">
+				<p class="question">&iquest;
+					Todas los proyectos deberán tener un enfoque comercial para buscar tener sustentabilidad?
+				</p>
+				
+				<p>
+					No necesariamente. Hay modelos de sustentabilidad que pueden estar asociados a la adopción del proyecto por parte de dependencias gubernamentales o de organizaciones de la sociedad civil. El programa de incubación/mentorship  para los desarrollos seleccionados ayudará a guiar a los participantes acerca del mejor modelo a implementar
+				</p>
+				
+				<div class="linea-r"></div>
+			</div>
+			
+			<div class="large-12 columns ">
+				<p class="question">&iquest;
+					Las aplicaciones o sitios tienen que ser desarrollados exactamente como se describen en el sitio conectadf.mx?
+				</p>
+				
+				<p>
+					No. El sitio intenta sembrar ideas y fomentar su discusión, pero la ejecución depende de la creatividad e innovación de los participantes. 
+				</p>
+				
+				<div class="linea-r"></div>
+			</div>	
+			
+			<div class="large-12 columns ">
+				<p class="question">&iquest;
+					Tengo que liberar el código desarrollado bajo licencia copyleft?
+				</p>
+				
+				<p>
+					No necesariamente. Las instituciones convocantes de ConectaDF abogamos por el aprovechamiento y difusión del software libre pero buscamos la participación más amplia posible; por lo que no restringimos la posibilidad de licenciamiento bajo copyright.
+				</p>
+				
+				<div class="linea-r"></div>
+			</div>
 		</div>											
 </div>
