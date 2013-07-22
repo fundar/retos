f4c17ff450783ba82d8630de2765c92a3d6f415e
Carlos Hugo Gonzalez Castell (Fundar) <carlos@fundar.org.mx>
2013-07-22 14:06:43 -0500

    tweet button single

--- www/applications/default/views/single.php
+++ www/applications/default/views/single.php
@@ -18,7 +18,7 @@
 							<li><p class="like votes like-post" value="<?php echo $post["post_id"];?>"><?php echo $post["votes"];?></p></li>
 							<li><p class="opinion"><?php echo $post["count"];?> Comentarios</p></li>
 							<li>
-								<a href="https://twitter.com/share" class="twitter-share-button" data-via="twitterapi" data-lang="en">Tweet</a>
+								<a href="https://twitter.com/share" class="twitter-share-button" data-text="<?php echo utf8_decode($post["title"]);?>" data-via="opendatamx" data-lang="en">Tweet</a>
 								<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 							</li>
 							</ul>		
