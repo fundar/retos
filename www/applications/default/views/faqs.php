a8e7803e3c6dba8bd6675e5575e0f6fa959ae93a
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2013-07-22 01:38:37 -0500

    faqs

--- www/applications/default/views/faqs.php
+++ www/applications/default/views/faqs.php
@@ -1,38 +1,53 @@
-<div class="large-12 columns top-single"> <!-- Contenido-->  
-	        <h3>Preguntas Frecuentes</h3>
+	<div class="large-12 columns top-single"> <!-- Contenido-->  
+	    <h3>Preguntas Frecuentes</h3>
+		
 		<div class="row">
-			<div class="large-12 columns ">				
-			<p class="cita">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lacus urna, consectetur id nibh eu, pulvinar accumsan sapien nullam non odio
-			</p>
-		        <div class="linea-quote"></div>
+			<div class="large-12 columns ">	
+						
+				<p class="cita">
+					.........
+				</p>
+			
+				<div class="linea-quote"></div>
 			</div>
 		</div>
+		
 		<div class="row">
 			<!-- preguntas-->  
 			<div class="large-12 columns ">
-				<p class="question">&iquest;Quisque fermentum neque libero, in pulvinar neque adipiscing vel? 					
+				<p class="question">&iquest;
+					Para qué un crear aplicaciones móviles en este reto si ya hay muchas actualmente?
 				</p>
+				
 				<p>
-Curabitur viverra, tellus a dapibus tristique, tortor est bibendum lacus, a sagittis tortor tortor sit amet odio. Nulla lorem tellus, porttitor sit amet sodales nec, tincidunt nec enim. Maecenas dapibus nibh tortor, in malesuada odio fermentum vel. Aliquam ac tincidunt purus, vel auctor orci. Praesent mollis, erat quis sodales aliquet, dui eros rhoncus enim, quis ullamcorper velit ipsum vitae tellus. Nulla semper metus et dignissim auctor. Vivamus in odio a nulla hendrerit sagittis ut sed leo.
+					La oferta actual de apps en materia de movilidad tienen un enfoque de muy corto plazo y no pueden generar valor agregado a partir de la reutilización de datos y metadatos relacionados. ConectaDF busca explorar la oportunidad de obtener beneficios de corto, mediano y largo plazo para la ciudadanía en general y no sólo de usuarios de smartphones.
 				</p>
-							<div class="linea-r"></div>
+				
+				<div class="linea-r"></div>
 			</div>
+			
 			<div class="large-12 columns ">
-				<p class="question">&iquest;Quisque fermentum neque libero, in pulvinar neque adipiscing vel. Nullam pretium at tortor vel fermentum? 					
+				<p class="question">&iquest;
+					¿Puedo comercializar el desarrollo que envíe?
 				</p>
+				
 				<p>
-Curabitur viverra, tellus a dapibus tristique, tortor est bibendum lacus, a sagittis tortor tortor sit amet odio. Nulla lorem tellus, porttitor sit amet sodales nec, tincidunt nec enim. Maecenas dapibus nibh tortor, in malesuada odio fermentum vel. Aliquam ac tincidunt purus, vel auctor orci. Praesent mollis, erat quis sodales aliquet, dui eros rhoncus enim, quis ullamcorper velit ipsum vitae tellus. Nulla semper metus et dignissim auctor. Vivamus in odio a nulla hendrerit sagittis ut sed leo.
+					Sí. De hecho las aplicaciones seleccionadas obtendrán acceso a un programa de incubación que garanticen su sustentabilidad y generen impacto de mediano y largo plazo. la única condición es que los datos generados a partir del uso de los desarrollos deberán estar disponibles como datos abiertos para las instituciones.
 				</p>
-							<div class="linea-r"></div>
+				
+				<div class="linea-r"></div>
 			</div>
+			
 			<div class="large-12 columns ">
-				<p class="question">&iquest;Quisque fermentum neque libero, in pulvinar neque adipiscing vel. Nullam pretium at tortor vel fermentum? 					
+				<p class="question">&iquest;
+					... 					
 				</p>
+				
 				<p>
-Curabitur viverra, tellus a dapibus tristique, tortor est bibendum lacus, a sagittis tortor tortor sit amet odio. Nulla lorem tellus, porttitor sit amet sodales nec, tincidunt nec enim. Maecenas dapibus nibh tortor, in malesuada odio fermentum vel. Aliquam ac tincidunt purus, vel auctor orci. Praesent mollis, erat quis sodales aliquet, dui eros rhoncus enim, quis ullamcorper velit ipsum vitae tellus. Nulla semper metus et dignissim auctor. Vivamus in odio a nulla hendrerit sagittis ut sed leo.
+					...
 				</p>
-							<div class="linea-r"></div>
+				
+				<div class="linea-r"></div>
 			</div>				
 		</div>											
 </div>
