aa4a40b3fb79c024430127e2e39fd8683447bcc7
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2013-07-22 02:26:50 -0500

    jquery ui

--- www/lib/themes/default/header.php
+++ www/lib/themes/default/header.php
@@ -11,7 +11,7 @@
 		
 		<script src="<?php echo $this->themePath; ?>/js/vendor/custom.modernizr.js"></script>
 		<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
-		<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.min.js"></script>
+		<script src="http://code.jquery.com/ui/1.8.7/jquery-ui.min.js"></script>
 	</head>
 	<body>
 	
