ed88f98684016d4c71102efe440661123cef8411
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2013-07-22 04:16:35 -0500

    convocatoria

--- www/applications/default/views/convocatoria.php
+++ www/applications/default/views/convocatoria.php
@@ -28,7 +28,7 @@
 				<br/>Web. Páginas y aplicaciones para navegadores.
 				<br/>Hardware. Proyectos que involucren sensores electrónicos de especificación abierta (arduino).
 				<br/>Híbrido. Alguna combinación de las anteriores categorías
-
+			
 				<br/><br/>- Los desarrollos deberán generar datos que ayuden a mejorar el transporte público y la movilidad urbana. El sito conectadf.mx servirá como plataforma de discusión e ideación de propuestas de generación de datos así como de estrategias que puedan incentivar la adopción masiva por parte de la ciudadanía.
 				<br/>- El reto queda abierto desde su publicación y hasta el 21 de agosto de 2013. Los desarrollos deberán ser enviados desde la siguiente URL [Carlos: agregar un conectadf.mx/submit con los campos necesarios: Nombre del representante, nombre de particpantes de equipo, ] y deberán incluir lo siguiente.
 				<br/>- Los datos de contacto de los participantes
@@ -36,26 +36,25 @@
 				<br/>- Un video demostrativo del desarrollo en funcionamiento. El video deberá enfocarse en mostrar el grado de ejecución de a idea y los detalles de funcionamiento del desarrollo. Aquellos videos que se enfoquen solamente en 
 				<br/>- Cualquier otro recurso de demostración que los participantes consideren necesarios para la mejor evaluación de la idea propuesta por parte del jurado. (se sugieren servicios de distribución de demos como Kickfolio o TestFlight).
 			</p>
-			
-			<p>
-			Mauris nisi urna, sodales a bibendum eget, suscipit ac turpis. Vivamus diam tellus, consectetur at dolor non, varius ultricies purus. Etiam varius nulla vitae lorem pulvinar sodales. Vivamus magna mi, dignissim fringilla urna vitae, fringilla pretium lectus. Aliquam pretium orci orci, at iaculis augue egestas ultricies. Pellentesque in massa quis massa consectetur varius. Nulla imperdiet tincidunt mi. Quisque vehicula venenatis suscipit. Suspendisse potenti. Nullam cursus dapibus eros quis malesuada. Praesent non eros massa.
-			</p>
-			<p>
-			Praesent ac posuere felis. Nunc odio dui, pretium eget enim at, placerat tincidunt eros. Vivamus rutrum est in est suscipit blandit. Ut suscipit at neque sed bibendum. In mattis sollicitudin nisl, id dapibus justo bibendum at. Mauris tempus purus mauris, eu eleifend est imperdiet non. Sed felis lectus, venenatis vitae euismod in, scelerisque ut odio. Etiam id urna condimentum neque euismod tincidunt ut quis felis. Suspendisse quis est eleifend, tristique metus eget, varius felis. Nam tempor enim sit amet turpis tincidunt condimentum. Maecenas vitae erat mauris. Nam purus felis, interdum sed tincidunt sed, dapibus vel quam.
-				
-			</p>
 		</div>		
 	</div>		
 	<div class="large-12 columns">
 		<div class="row">
 			 <div class="small-11 small-centered columns"><img src="<?php echo $this->themePath; ?>/img/destacado.jpg" style="margin: 50px auto 30px;"></div>
 	        </div>
+	        
+	        <p><strong>Incentivos</strong></p>
+
 			<p>
-			Mauris nisi urna, sodales a bibendum eget, suscipit ac turpis. Vivamus diam tellus, consectetur at dolor non, varius ultricies purus. Etiam varius nulla vitae lorem pulvinar sodales. Vivamus magna mi, dignissim fringilla urna vitae, fringilla pretium lectus. Aliquam pretium orci orci, at iaculis augue egestas ultricies. Pellentesque in massa quis massa consectetur varius. Nulla imperdiet tincidunt mi. Quisque vehicula venenatis suscipit. Suspendisse potenti. Nullam cursus dapibus eros quis malesuada. Praesent non eros massa.
+				- Las propuestas serán juzgadas de acuerdo a los criterios de impacto potencial sobre la problemática particular a la que se dirigen, creatividad de la propuesta para la generación/análisis de datos, uso innovador de la tecnología, así como la calidad general de ejecución del desarrollo.
+
+				<br/>- El jurado estará conformado por personalidades reconocidas en los ámbitos de las políticas públicas de movilidad y la innovación tecnológica. El fallo del jurado será inapelable y público el día 24 de agosto en el Laboratorio para la ciudad.
+
+				<br/>- Los participantes cuyos proyectos sean seleccionados obtendrán acceso a un programa de incubación/mentorship otorgado por las instituciones convocantes el cual permitirá perfeccionar el desarrollo, formar estrategias de adopción e implementación, así como garantizar acceso a infraestructura y recursos necesarios para maximizar su impacto. Dicho programa tendrá una duración mínima de 3 meses y máxima de un año contados a partir de la publicación del veredicto. 
+
+				<br/>- No existe un número de lugares predeterminado. Se otorgará acceso al programa de incubación/mentorship a todos aquellos proyectos que alcanzen los critrerios descritos anteriormente. 
+
+				<br/>- Los participantes cuyos proyectos sean seleccionados podrán hacer uso comercial de los desarrollos y datos generados al final del programa de incubación/mentorship  bajo la única condición de otorgar a las instituciones convocantes acceso automatizado a los datos generados por las aplicaciones. 
 			</p>				
-						<p>
-			Praesent ac posuere felis. Nunc odio dui, pretium eget enim at, placerat tincidunt eros. Vivamus rutrum est in est suscipit blandit. Ut suscipit at neque sed bibendum. In mattis sollicitudin nisl, id dapibus justo bibendum at. Mauris tempus purus mauris, eu eleifend est imperdiet non. Sed felis lectus, venenatis vitae euismod in, scelerisque ut odio. Etiam id urna condimentum neque euismod tincidunt ut quis felis. Suspendisse quis est eleifend, tristique metus eget, varius felis. Nam tempor enim sit amet turpis tincidunt condimentum. Maecenas vitae erat mauris. Nam purus felis, interdum sed tincidunt sed, dapibus vel quam.
-				
-			</p>
 	</div>										
 </div>
