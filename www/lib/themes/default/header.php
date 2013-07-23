c80a6cb70b2e600464e57d7ba2dd723e4e08dad6
Carlos Hugo Gonzalez Castell (Fundar) <carlos@fundar.org.mx>
2013-07-23 16:10:24 -0500

    sube tu idea hide

--- www/lib/themes/default/header.php
+++ www/lib/themes/default/header.php
@@ -125,7 +125,7 @@
 			
 		<li><a href="/" class="button<?php echo (!segment(0)) ? " active-btn" : "";?>">Retos</a></li>
 		<li><a href="/convocatoria" class="button<?php echo (segment(0) == "convocatoria") ? " active-btn" : "";?>">Convocatoria</a></li>
-		<!-- <li><a href="#" class="button">Sube tu idea</a></li> -->
+		<!-- <li><a href="/add" class="button<?php echo (segment(0) == "add") ? " active-btn" : "";?>">Sube tu idea</a></li> -->
 		<li><a href="/faqs" class="button<?php echo (segment(0) == "faqs") ? " active-btn" : "";?>">Preguntas frecuentes</a></li>
 		</ul>
 		</div>
