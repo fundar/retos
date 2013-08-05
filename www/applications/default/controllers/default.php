3e6949df202292685aaee0d7c0970b83e95b9065
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2013-08-05 15:11:59 -0500

    default controller

--- www/applications/default/controllers/default.php
+++ www/applications/default/controllers/default.php
@@ -36,6 +36,7 @@ class Default_Controller extends ZP_Controller {
 		
 		if($user) {
 			if($user[0]["admin"] == "t") {
+				$vars["user"]     = $this->isUser();
 				$vars["projects"] = $this->Default_Model->getProjects();
 				$vars["view"]     = $this->view("projects", true);
 				
