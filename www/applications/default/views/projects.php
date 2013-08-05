7982b00682ad88b5c7cf4576867361d9130d81f4
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2013-08-05 15:18:03 -0500

    projects

--- www/applications/default/views/projects.php
+++ www/applications/default/views/projects.php
@@ -9,11 +9,19 @@
 					<div class="row">
 						<p><strong><?php echo utf8_decode($project["title"]);?></strong></p>
 						
-						<p><?php echo utf8_decode($project["descr"]);?></p>
+						<p>Descripci&oacute;n: <?php echo utf8_decode($project["descr"]);?></p>
 						
 						<p>
+							Categor&iacute;a: <?php echo $project["name_category"];?><br/>
 							Url del video: <?php echo $project["url_video"];?><br/>
 							Url del demo: <?php echo $project["url_demo"];?><br/>
+						</p>
+						
+						<p>
+							Representante: <?php echo utf8_decode($project["name"]);?><br/>
+							Correo: <?php echo utf8_decode($project["email"]);?><br/><br/>
+							
+							Integrantes: <?php echo utf8_decode($project["names"]);?>
 						</p>
 					</div>
 				</div>
