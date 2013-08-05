2ca9bc5d206044b8a665d01e8a9ddfd500fe70fd
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2013-08-05 12:47:15 -0500

    convocatoria

--- www/applications/default/views/convocatoria.php
+++ www/applications/default/views/convocatoria.php
@@ -52,9 +52,11 @@
 	</div>	
 		
 	<div class="large-12 columns">
-		<p>
-			Como resultado de un gran número de solicitudes, se ha abierto una nueva categoría  de participación para desarrollos que ya existan en el mercado en tiendas en línea (appstore, Google Play, etc.) y cuyos creadores tienen el interés de abrir sus datos. Las aplicaciones de esta categoría no competirán con el resto de las categorías y de ser seleccionadas recibirán acceso a campañas de difusión con una estrategia integral de medios. Como en el caso de las otras categorías, se requerirá el otorgamiento de acceso automatizado a los datos asociados a la movilidad urbana que el desarrollo genere.
-		</p>
+		<div class="row">
+			<p>
+				Como resultado de un gran número de solicitudes, se ha abierto una nueva categoría  de participación para desarrollos que ya existan en el mercado en tiendas en línea (appstore, Google Play, etc.) y cuyos creadores tienen el interés de abrir sus datos. Las aplicaciones de esta categoría no competirán con el resto de las categorías y de ser seleccionadas recibirán acceso a campañas de difusión con una estrategia integral de medios. Como en el caso de las otras categorías, se requerirá el otorgamiento de acceso automatizado a los datos asociados a la movilidad urbana que el desarrollo genere.
+			</p>
+		</div>
 	</div>
 	
 	<div class="large-12 columns">
