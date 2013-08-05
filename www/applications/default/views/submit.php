f8b7864c61807e38d23d9f2ca01b1c1996345638
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2013-08-05 13:47:49 -0500

    descrupción

--- www/applications/default/views/submit.php
+++ www/applications/default/views/submit.php
@@ -18,22 +18,25 @@
 	
 	<br />
 	
-	<p>Asegúrate de explicar como contribuyes a la problemática de la movilidad urbana así como el tipo de datos asociados que se generarían, analizarían o difundirían. Explica las funcionalidades con detalle y el valor agregado par los usuarios, los convocantes u otro actor relacionado con la problemática. Comenta qué tipo de modelo de sustentabilidad podría aplicarse a la propuesta.</p>
-	
 	Nombre del representante*: <input type="text" name="name" value="<?php echo (isset($_POST["name"])) ? $_POST["name"]: ""; ?>" /><br/>
 	Correo del representante*: <input type="text" name="email" value="<?php echo (isset($_POST["email"])) ? $_POST["email"]: ""; ?>" /><br/>
 	Nombres de los integrantes*: <textarea name="names"><?php echo (isset($_POST["names"])) ? $_POST["names"]: ""; ?></textarea><br/>
 	
 	Nombre del proyecto*: <input type="text" name="title" value="<?php echo (isset($_POST["title"])) ? $_POST["title"]: ""; ?>" /><br/>
-	Descripci&oacute;n*: <textarea name="descr" class="editme"><?php echo (isset($_POST["descr"])) ? $_POST["descr"]: ""; ?></textarea><br/>
+	Descripci&oacute;n*: <textarea name="descr" class="editme"><?php echo (isset($_POST["descr"])) ? $_POST["descr"]: ""; ?></textarea>
+	<p>Asegúrate de explicar como contribuyes a la problemática de la movilidad urbana así como el tipo de datos asociados que se generarían, analizarían o difundirían. Explica las funcionalidades con detalle y el valor agregado par los usuarios, los convocantes u otro actor relacionado con la problemática. Comenta qué tipo de modelo de sustentabilidad podría aplicarse a la propuesta.</p>
 	
+	<br/>
+	
+	Url del video explicando el proyecto*: <input type="text" name="url-video" value="<?php echo (isset($_POST["url-video"])) ? $_POST["url-video"]: ""; ?>" /><
 	<p>Trata de que tu video no pase de 3 minutos. Intenta ejemplificar el uso cotidiano y no dejes fuera los detalles importantes de valor agreado que distingan a tu propuesta.</p>
-	Url del video explicando el proyecto*: <input type="text" name="url-video" value="<?php echo (isset($_POST["url-video"])) ? $_POST["url-video"]: ""; ?>" /><br/>
 	
+	<br/>
+	
+	Url del demo o algun otro recurso demostrativo: <input type="text" name="url-demo" value="<?php echo (isset($_POST["url-demo"])) ? $_POST["url-demo"]: ""; ?>" />
 	<p>El demo es opcional pero si se incluye es un factor que puede ayudar positivamente a la decisión de los jueces (sugerimos servicios de distribución de demos como Kickfolio o TestFlight).</p>
-	Url del demo o algun otro recurso demostrativo: <input type="text" name="url-demo" value="<?php echo (isset($_POST["url-demo"])) ? $_POST["url-demo"]: ""; ?>" /><br/>
 	
-	<p>Sí tu aplicación ya está en algúna tienda en línea (appstore, Google Play, etc) debes usar la categoría Apps pre-existentes)</p>
+	<br/>
 	Categor&iacute;a*: 
 	<select name="category_id">
 		<?php foreach($categories as $category) { ?>
@@ -42,6 +45,7 @@
 			</option>
 		<?php } ?>
 	</select>
+	<p>El demo es opcional pero si se incluye es un factor que puede ayudar positivamente a la decisión de los jueces (sugerimos servicios de distribución de demos como Kickfolio o TestFlight).</p>
 	
 	<br/><br/><br/>
 	<p>
