72aa8dba541278d7ae67d42d581e996b546fbc03
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2013-08-19 11:04:12 -0500

    message submit successful

--- www/applications/default/views/submit-successful.php
+++ www/applications/default/views/submit-successful.php
@@ -1,1 +1,12 @@
-<br/><br/><p>Gracias por participar, evaluaremos tu proyecto</p>
+<br/><br/>
+
+<p>
+Gracias por enviar tu proyecto a ConectaDF.
+Los jueces evaluar&aacute;n tu propuesta de acuerdo a las bases. 
+De requerir informaci&oacute;n adicional acerca de tu proyecto nos pondremos en contacto contigo, de manera que te pedimos est&eacute;s pendiente a partir del 22 de agosto y hasta el 27, 
+d&iacute;a en que ser&aacute;n anunciados los proyectos seleccionados.<br/><br/>
+
+Saludos,<br/>
+
+El equipo OpenDataMx
+</p>
