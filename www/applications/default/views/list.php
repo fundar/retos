c61fb8f11d642a909ff1874118d1752054a451bc
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2013-08-26 13:09:57 -0500

    listado ol

--- www/applications/default/views/list.php
+++ www/applications/default/views/list.php
@@ -4,17 +4,15 @@
 		<h3>Proyectos</h3>
 		
 		<div class="row">
-			</ul>
+			<ol>
 				<?php foreach($projects as $key => $project) { ?>
 					<li>
-						<?php echo $key+1;?>.- 
-						
 						<a href="/project/<?php echo $key;?>">
 							<?php echo utf8_decode($project["title"]);?>
 						</a>
 					</li>
 				<?php } ?>
-			</ul>
+			</ol>
 		</div>
 	</div>
 <?php } else { ?>
