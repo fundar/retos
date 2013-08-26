07e482c908e8e9e409d8f1a4b404840e738a4ef5
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2013-08-26 13:38:52 -0500

    submit

--- www/applications/default/controllers/default.php
+++ www/applications/default/controllers/default.php
@@ -92,9 +92,8 @@ class Default_Controller extends ZP_Controller {
 	}
 	
 	public function submit() {
-		header('Location:' . get("webURL"));
+		//header('Location:' . get("webURL"));
 		
-		/*
 		$this->title("Sube tu proyecto");
 		
 		$vars["user"] = $this->isUser();
@@ -119,7 +118,6 @@ class Default_Controller extends ZP_Controller {
 		}
 		
 		$this->render("content", $vars);
-		*/
 	}
 	
 	public function order($type = false) {
